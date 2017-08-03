a90a84e6d2d7bc7ea12bbbbca8689148fc4c8627
ny1am <ny1am.lviv.ua@gmail.com>
2017-08-03 16:39:17 +0300

    basic server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
