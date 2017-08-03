ff33708f256aedacadc83411651e5585815e15f4
ny1am <ny1am.lviv.ua@gmail.com>
2017-08-03 18:12:11 +0300

    added full calendar

--- public/client.js
+++ public/client.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor: ['#f2f2f2', '#4BBFC3']
+    sectionsColor: ['#4BBFC3', '#f2f2f2']
+  });
+  $('#calendar').fullCalendar({
+    locale: 'uk',
+    height: 'parent',
+    events: [
+      {
+        title: 'їбаш невпинно',
+        dow: [0, 1, 2, 3, 4, 5, 6],
+        order: 0,
+      },
+      {
+        title: 'не їж солодкого',
+        dow: [0, 1, 2, 3, 4, 5, 6],
+        order: 1
+      },
+      {
+        title: 'шоколадний макарун зі "Світу кави"',
+        dow: [0, 4],
+        color: '#fecc0c',
+        order: 2
+      },
+      {
+        title: 'м\'ятний макарун зі "Світу кави"',
+        dow: [0],
+        color: '#fecc0c',
+        order: 3
+      },
+    ],
+    eventOrder: 'order',
+    eventLimit: 2
   });
 });
