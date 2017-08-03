f02f31a7e974be67b8e080d137a9f07c17c24912
ny1am <ny1am.lviv.ua@gmail.com>
2017-08-03 16:58:55 +0300

    added favicon

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const path = require('path');
 
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(path.join(__dirname + '/public/img/favicon.ico')));
 
 app.get('/', function(request, response) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
